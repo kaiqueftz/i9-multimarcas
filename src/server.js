48c7c95cc2b5e97017f17b4aea651ec2a214c2e5
starwars-api <kaiqueln03@gmail.com>
2024-12-17 14:37:12 -0300

    Atualização

--- src/server.js
+++ src/server.js
@@ -164,8 +164,7 @@ app.post('/login', async (req, res) => {
     }
 });
 
-// Inicializar o servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
